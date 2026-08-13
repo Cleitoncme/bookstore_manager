b160ded73cf69fec115ae72a141e4964f28d86d1
Cleitoncme <cleitonjlc@gmail.com>
2026-08-12 23:41:29 -0300

    Atualização eslint / package e package-lock

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,14 @@
-import eslint from '@eslint/js'
-import { defineConfig } from 'eslint/config'
-import eslintPluginImport from 'eslint-plugin-import'
-import prettierPlugin from 'eslint-plugin-prettier'
-import tseslint from 'typescript-eslint'
+/* eslint-disable import/no-unresolved */
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import eslintPluginImport from 'eslint-plugin-import';
+import prettierPlugin from 'eslint-plugin-prettier';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 const config = defineConfig(
   {
-    ignores: ['dist/**', 'node_modules/**', 'src/_experiments/**']
+    ignores: ['dist/**', 'node_modules/**', 'src/_experiments/**'],
   },
   eslint.configs.recommended,
   tseslint.configs.strictTypeChecked,
@@ -15,27 +17,27 @@ const config = defineConfig(
     languageOptions: {
       parserOptions: {
         projectService: {
-          allowDefaultProject: ['*.js', '*.mjs', 'vitest.config.ts']
+          allowDefaultProject: ['*.js', '*.mjs', 'vitest.config.ts'],
         },
-        tsconfigRootDir: import.meta.dirname
+        tsconfigRootDir: import.meta.dirname,
       },
       sourceType: 'module',
       globals: {
-        node: true
-      }
+        ...globals.node,
+      },
     },
 
     plugins: {
       import: eslintPluginImport,
-      prettier: prettierPlugin
+      prettier: prettierPlugin,
     },
 
     settings: {
       'import/resolver': {
         node: {
-          extensions: ['.js', '.jsx', '.ts', '.tsx']
-        }
-      }
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+      },
     },
 
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
@@ -45,7 +47,7 @@ const config = defineConfig(
       ...eslintPluginImport.configs.typescript.rules,
 
       '@typescript-eslint/no-extraneous-class': ['off'],
-      '@typescript-eslint/interface-name-prefix': 'off',
+
       'import/no-absolute-path': 'error',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-namespace': 'off',
@@ -55,8 +57,8 @@ const config = defineConfig(
         {
           argsIgnorePattern: '_',
           varsIgnorePattern: '_',
-          caughtErrorsIgnorePattern: '_'
-        }
+          caughtErrorsIgnorePattern: '_',
+        },
       ],
       'import/no-unresolved': 'error',
       'import/named': 'off',
@@ -67,17 +69,17 @@ const config = defineConfig(
             'builtin',
             'external',
             'internal',
-            ['parent', 'sibling', 'index']
+            ['parent', 'sibling', 'index'],
           ],
           'newlines-between': 'always',
           alphabetize: {
             order: 'asc',
-            caseInsensitive: true
-          }
-        }
-      ]
-    }
-  }
-)
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  },
+);
 
-export default config
+export default config;
